ecb03ad68490a9c0377747cfd034cc3452a3043d
annaboye <boye.anna@gmail.com>
2023-12-06 11:31:35 +0100

    styles: add mixins varibels and layout component

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,22 +3,31 @@ import { Startpage } from "./views/Startpage/Startpage";
 import { Error } from "./views/Error/Error";
 import { Game } from "./views/Game/Game";
 import { About } from "./views/About/About";
+import { Layout } from "./views/Layout";
 
 
 export const router = createBrowserRouter([
     {
       path: "/",
-      element: <Startpage></Startpage>,
+      element: <Layout></Layout>,
       errorElement: <Error></Error>,
+    children: [
+        {
+          path: "/",
+          element: <Startpage></Startpage>,
+          index: true,
+        },
+        {
+            path: "/game",
+            element: <Game></Game>,
+            errorElement: <Error></Error>,
+          },
+          {
+            path: "/about",
+            element: <About></About>,
+            errorElement: <Error></Error>,
+          },
+      ],
     },
-    {
-      path: "/game",
-      element: <Game></Game>,
-      errorElement: <Error></Error>,
-    },
-    {
-      path: "/about",
-      element: <About></About>,
-      errorElement: <Error></Error>,
-    },
+ 
   ]);
--- src/views/Startpage/Startpage.tsx
+++ src/views/Startpage/Startpage.tsx
@@ -1,7 +1,14 @@
+import "./Startpage.scss"
+
 export const Startpage= () => {
     return (
       <>
-        <div>startpage works!</div>
+        <div className="bg-wrapper">
+            <div className="img-wrapper">
+            <img src="https://cdn.pixabay.com/photo/2016/08/15/00/50/pokeball-1594373_640.png" alt="pokemonball" />
+            </div>
+            
+        </div>
       </>
     );
   };
