e6300a92b7a19925775ee6e951e5e8c7620ede22
annaboye <boye.anna@gmail.com>
2023-12-08 08:50:48 +0100

    feat: add context provider in layout

--- src/views/Layout.tsx
+++ src/views/Layout.tsx
@@ -1,15 +1,27 @@
 import { Outlet } from "react-router-dom";
+import { Nav } from "../components/Nav/Nav";
+import { GameContext, GameDispatchContext } from "../contexts/CurrentGameContext";
+import { gameReducer, initialState } from "../reducers/CurrentGameReducer";
+import { useReducer } from "react";
+
+
 
 export const Layout = () => {
+  const [currentGame, dispatch] = useReducer(gameReducer, initialState);
     return (
       <>
-        <header>
-        </header>
+      <GameContext.Provider value={currentGame}>
+      <GameDispatchContext.Provider value={dispatch}>
+      <header>
+      <Nav></Nav>
+      </header>
         <main>
           <Outlet></Outlet>
         </main>
-        <footer>
-        </footer>
+      <footer>
+      </footer>
+      </GameDispatchContext.Provider>
+      </GameContext.Provider>
       </>
     );
   };
