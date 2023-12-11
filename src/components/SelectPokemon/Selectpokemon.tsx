374c833e6b45a02370b01087e0888579bffed3c6
annaboye <boye.anna@gmail.com>
2023-12-11 09:13:40 +0100

    feat: fetch and select pokemons

--- src/components/SelectPokemon/Selectpokemon.tsx
+++ src/components/SelectPokemon/Selectpokemon.tsx
@@ -1,7 +1,33 @@
-export const SelectPokemon= () => {
-    return (
-      <>
-        <div>Select pokemon!</div>
-      </>
-    );
+import { useEffect, useState } from "react";
+import { getPokemonData } from "../../services/getPokemon";
+import { IPokemon } from "../../models/IPokemon";
+import "./SelectPokemon.scss"
+
+interface SelectPokemonProps {
+  selectPokemon: (selectedPokemon: IPokemon) => void;
+}
+
+export const SelectPokemon= ({selectPokemon}: SelectPokemonProps) => {
+  const [pokemons, setPokemons] = useState<IPokemon[]>([]);
+    
+  useEffect(()=> {
+      const fetchData = async () => {
+          try {
+            const pokemonList = await getPokemonData();
+            setPokemons(pokemonList);
+          } catch (error) {
+            console.error('Error fetching Pokemon data:', error);
+          }
+        };
+      
+        fetchData();
+
+  }, [])
+  return (
+    <>
+      <div className="poki-wrapper">{pokemons.map((pokemon)=>( <img onClick={() => selectPokemon(pokemon)} src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.evolves_from_species_id}.png`}
+      alt="pokemon" key={pokemon.id} />
+      ))}</div>
+    </>
+  );
   };
