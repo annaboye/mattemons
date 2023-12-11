9d5e6fd8e988f52406be62e061f605833433a898
annaboye <boye.anna@gmail.com>
2023-12-11 10:58:53 +0100

    feat: able to select pokemon. using reducer and context

--- src/components/SelectPokemon/Selectpokemon.tsx
+++ src/components/SelectPokemon/Selectpokemon.tsx
@@ -1,14 +1,23 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { getPokemonData } from "../../services/getPokemon";
 import { IPokemon } from "../../models/IPokemon";
 import "./SelectPokemon.scss"
+import { GameContext, GameDispatchContext } from "../../contexts/CurrentGameContext";
 
 interface SelectPokemonProps {
-  selectPokemon: (selectedPokemon: IPokemon) => void;
+  pokemonIsSelected: () => void;
 }
 
-export const SelectPokemon= ({selectPokemon}: SelectPokemonProps) => {
+export const SelectPokemon= ({pokemonIsSelected}: SelectPokemonProps) => {
   const [pokemons, setPokemons] = useState<IPokemon[]>([]);
+  const dispatch = useContext(GameDispatchContext);
+  const currentGame= useContext(GameContext);
+
+  const selectPokemon = (pokemon: IPokemon)=> {
+    dispatch({type: "SET_SELECTED_POKEMON",
+    payload: pokemon})
+    pokemonIsSelected();
+  }
     
   useEffect(()=> {
       const fetchData = async () => {
@@ -23,10 +32,13 @@ export const SelectPokemon= ({selectPokemon}: SelectPokemonProps) => {
         fetchData();
 
   }, [])
+
+ 
   return (
     <>
-      <div className="poki-wrapper">{pokemons.map((pokemon)=>( <img onClick={() => selectPokemon(pokemon)} src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.evolves_from_species_id}.png`}
-      alt="pokemon" key={pokemon.id} />
+    <p>{currentGame.player.playerName } nu ska du välja din pokemon som du vill ska utvecklas till nästa nivå:</p>
+      <div className="poki-wrapper">{pokemons.map((pokemon)=>( <div className="img-container"><img onClick={() => selectPokemon(pokemon)} src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.evolves_from_species_id}.png`}
+      alt="pokemon" key={pokemon.id} /> </div>
       ))}</div>
     </>
   );
