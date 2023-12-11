e5052ba42e00dc8eb7868ea4ffcfe4742b83f7eb
annaboye <boye.anna@gmail.com>
2023-12-11 09:48:06 +0100

    refactor: moved context provider to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-
+import { GameContext, GameDispatchContext } from "../src/contexts/CurrentGameContext";
+import { gameReducer, initialState } from "../src/reducers/CurrentGameReducer";
+import { useReducer } from "react";
 import { RouterProvider } from 'react-router-dom'
 import './App.scss'
-
 import { router } from './Router'
 
 function App() {
+  const [currentGame, dispatch] = useReducer(gameReducer, initialState);
   
 
   return (
-    <>
-    <RouterProvider router={router}></RouterProvider>
-  </>
+    <GameContext.Provider value={currentGame}>
+     <GameDispatchContext.Provider value={dispatch}>
+      <RouterProvider router={router}></RouterProvider>
+     </GameDispatchContext.Provider>
+    </GameContext.Provider>
   )
 }
 
--- src/views/Layout.tsx
+++ src/views/Layout.tsx
@@ -1,17 +1,12 @@
 import { Outlet } from "react-router-dom";
 import { Nav } from "../components/Nav/Nav";
-import { GameContext, GameDispatchContext } from "../contexts/CurrentGameContext";
-import { gameReducer, initialState } from "../reducers/CurrentGameReducer";
-import { useReducer } from "react";
+
 
 
 
 export const Layout = () => {
-  const [currentGame, dispatch] = useReducer(gameReducer, initialState);
     return (
       <>
-      <GameContext.Provider value={currentGame}>
-      <GameDispatchContext.Provider value={dispatch}>
       <header>
       <Nav></Nav>
       </header>
@@ -20,8 +15,6 @@ export const Layout = () => {
         </main>
       <footer>
       </footer>
-      </GameDispatchContext.Provider>
-      </GameContext.Provider>
       </>
     );
   };
