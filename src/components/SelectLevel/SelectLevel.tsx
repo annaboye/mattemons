65752cabb9996edbea5b775e7b641c2898c23532
annaboye <boye.anna@gmail.com>
2023-12-15 09:32:02 +0100

    feat: add tabindex and focus

--- src/components/SelectLevel/SelectLevel.tsx
+++ src/components/SelectLevel/SelectLevel.tsx
@@ -30,21 +30,21 @@ export const SelectLevel= ({levelIsSelected}: ISelectLevelProps) => {
     <div><img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${currentGame.selectedPokemon.evolves_from_species_id}.png`} alt="pokemon" /></div>
       <div>Välj räknesätt:</div>
         <div>
-        <button className={`toggle ${currentGame.level.calculationMethod=== "+"? "active" : ""}`} onClick={() => setCalculationMethod("+")}><i className="icon fa-solid fa-plus"></i>
+        <button tabIndex= {0} className={`toggle ${currentGame.level.calculationMethod=== "+"? "active" : ""}`} onClick={() => setCalculationMethod("+")}><i className="icon fa-solid fa-plus"></i>
         </button>
-        <button className={`toggle ${currentGame.level.calculationMethod=== "-"? "active" : ""}`} onClick={() => setCalculationMethod("-")}><i className="icon fa-solid fa-minus"></i></button>
-        <button className={`toggle ${currentGame.level.calculationMethod=== "*"? "active" : ""}`} onClick={() => setCalculationMethod("*")}><i className="icon fa-solid fa-xmark"></i></button>
+        <button tabIndex= {0} className={`toggle ${currentGame.level.calculationMethod=== "-"? "active" : ""}`} onClick={() => setCalculationMethod("-")}><i className="icon fa-solid fa-minus"></i></button>
+        <button tabIndex= {0} className={`toggle ${currentGame.level.calculationMethod=== "*"? "active" : ""}`} onClick={() => setCalculationMethod("*")}><i className="icon fa-solid fa-xmark"></i></button>
         
-        <button className={`toggle ${currentGame.level.calculationMethod=== "/"? "active" : ""}`} onClick={() => setCalculationMethod("/")}><i className="icon fa-solid fa-divide"></i></button>
+        <button tabIndex= {0} className={`toggle ${currentGame.level.calculationMethod=== "/"? "active" : ""}`} onClick={() => setCalculationMethod("/")}><i className="icon fa-solid fa-divide"></i></button>
       </div>
        
        <div>Välj svårighetsgrad:</div>
       <div className="selectlevel">
-        <button className={`toggle-long ${currentGame.level.numberMax===5 ? "active" : ""}`} onClick={() => setLevel(5)}>Nivå 1: Siffror upp till 5</button>
-        <button className={`toggle-long ${currentGame.level.numberMax===10 ? "active" : ""}`} onClick={() => setLevel(10)}>Nivå 2: Siffror upp till 10</button>
-        <button className={`toggle-long ${currentGame.level.numberMax===20 ? "active" : ""}`} onClick={() => setLevel(20)}>Nivå 3: Siffror upp till 20</button>
+        <button tabIndex= {0} className={`toggle-long ${currentGame.level.numberMax===5 ? "active" : ""}`} onClick={() => setLevel(5)}>Nivå 1: Siffror upp till 5</button>
+        <button tabIndex= {0} className={`toggle-long ${currentGame.level.numberMax===10 ? "active" : ""}`} onClick={() => setLevel(10)}>Nivå 2: Siffror upp till 10</button>
+        <button tabIndex= {0}className={`toggle-long ${currentGame.level.numberMax===20 ? "active" : ""}`} onClick={() => setLevel(20)}>Nivå 3: Siffror upp till 20</button>
       </div>
-      <button className="btn-start"type="button" onClick={startTheGame} disabled={currentGame.level.numberMax=== 0 || currentGame.level.calculationMethod === "" }>START</button>
+      <button  tabIndex= {0}className="btn-start"type="button" onClick={startTheGame} disabled={currentGame.level.numberMax=== 0 || currentGame.level.calculationMethod === "" }>START</button>
       </div>
       </>
     );
