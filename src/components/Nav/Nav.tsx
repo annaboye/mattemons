e4fd0ecbb7040018b5f2a2386c3802789a4de561
annaboye <boye.anna@gmail.com>
2023-12-20 10:44:22 +0100

    feat: new nav

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,17 +1,28 @@
 import { Link } from "react-router-dom";
+import { MdOutlineCatchingPokemon } from "react-icons/md";
 import "./Nav.scss"
+import { Modal } from "../Modal/Modal";
+import { useState } from "react";
 
 export const Nav= () => {
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => setModalOpen(true);
+  const closeModal = () => setModalOpen(false);
     return (
     <header className="header">
       <ul className="menu">
         <li>
-        <Link to={"/about"}>
-                  info
-                </Link>
+        <button onClick={openModal} tabIndex={0} aria-label="öppna navigering">
+        <MdOutlineCatchingPokemon className="navicon"  aria-label="öppna navigering" />
+        </button>
         </li>
-   
       </ul>
+      <Modal isOpen={isModalOpen} onClose={closeModal}>
+        <h2>Vart vill du gå?</h2>
+        <Link to={"/"}><button className="long-btn">STARTSIDA</button></Link>
+        <Link to={"/collection"}><button className="long-btn">MIN POKEMONSAMLING</button></Link>
+      </Modal>
     </header>
    )
 
