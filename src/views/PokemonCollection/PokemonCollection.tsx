2bbe0f16c487e8ff4c62a954e10806c9608e4cc9
annaboye <boye.anna@gmail.com>
2023-12-25 16:51:11 +0100

    feat:  change to IPokemon array in IPlayer, show pokemonname in collection

--- src/views/PokemonCollection/PokemonCollection.tsx
+++ src/views/PokemonCollection/PokemonCollection.tsx
@@ -7,13 +7,13 @@ import { useNavigate } from "react-router-dom";
 export const PokemonCollection = () =>{
     const currentGame= useContext(GameContext);
     const navigate= useNavigate();
-
     return(
     <>
     <div className="collection-wrapper">
     <div className="content-wrapper">
-    <div className="poki-wrapper">{currentGame.player.pokemonsIdList.map((pokemonId)=>( <div key={pokemonId} className="img-container"><img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`}
-    alt="pokemon"/></div>))}
+    <div className="pokis-wrapper">{currentGame.player.pokemonList.map((pokemon)=>(<div className="pokemon"> <div key={pokemon.id} className="img-container"><img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
+    alt="pokemon"/></div>
+    <span>{pokemon.name}</span></div>))}
     </div>
     <button className="play-btn" onClick={ ()=>{ navigate('/selectpokemon')} }>spela igen</button>
     <button className="cancel-btn" onClick={()=>{ navigate('/')}}>avsluta</button>
