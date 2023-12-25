0a7e5dbe2621b35a78b98d46208ff7df20b602b5
annaboye <boye.anna@gmail.com>
2023-12-25 16:28:27 +0100

    feat: add pokemoncollection view

--- src/views/PokemonCollection/PokemonCollection.tsx
+++ src/views/PokemonCollection/PokemonCollection.tsx
@@ -1,6 +1,22 @@
+import { useContext } from "react";
 import "./PokemonCollection.scss";
+import { GameContext } from "../../contexts/CurrentGameContext";
+import { useNavigate } from "react-router-dom";
 
 
 export const PokemonCollection = () =>{
-    return(<><p>Pokemoncollection works!</p></>)
+    const currentGame= useContext(GameContext);
+    const navigate= useNavigate();
+
+    return(
+    <>
+    <div className="collection-wrapper">
+    <div className="content-wrapper">
+    <div className="poki-wrapper">{currentGame.player.pokemonsIdList.map((pokemonId)=>( <div key={pokemonId} className="img-container"><img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId}.png`}
+    alt="pokemon"/></div>))}
+    </div>
+    <button className="play-btn" onClick={ ()=>{ navigate('/selectpokemon')} }>spela igen</button>
+    <button className="cancel-btn" onClick={()=>{ navigate('/')}}>avsluta</button>
+    </div>
+    </div></>)
 }
