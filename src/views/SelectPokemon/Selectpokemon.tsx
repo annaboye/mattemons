fd3da21487bc2181bd5b0c6fe5c56ca367308b13
annaboye <boye.anna@gmail.com>
2024-01-03 09:10:17 +0100

    feat: aria-label for accessibility

--- src/views/SelectPokemon/Selectpokemon.tsx
+++ src/views/SelectPokemon/Selectpokemon.tsx
@@ -45,7 +45,7 @@ export const SelectPokemon= () => {
     <>
     <div className="select-wrapper">
       <div className="player">Hej {currentGame.player.playerName}! <p> Fånga din pokemon som ska utvecklas till nästa nivå: </p></div> 
-      <div className="poki-wrapper">{pokemons.map((pokemon)=>( <div key={pokemon.id} className="img-container"><img onClick={() => selectPokemon(pokemon)} src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.evolves_from_species_id}.png`}
+      <div className="poki-wrapper">{pokemons.map((pokemon)=>( <div aria-label="välj pokemon" role="button"key={pokemon.id} className="img-container" tabIndex={0} onClick={() => selectPokemon(pokemon)}><img  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.evolves_from_species_id}.png`}
       alt="pokemon"  />
       </div>
       ))}
